b52ee1ab1a45a88a5b0695a8cc2c152d21f3e974
jagadeshchilla <chillajagadesh68@gmail.com>
2025-06-26 11:25:52 +0530

    data validation is completed

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from src.datascience_project import logger
+from src.datascience_project.components.data_ingestion import DataIngestionTrainingPipeline
+from src.datascience_project.pipeline.data_validation import DataValidationTrainingPipeline
 
-logger.info("Welcome to my custom logging")
+STAGE_NAME="Data Ingestion stage"
 
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion=DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME="Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_validation=DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/datascience_project/components/data_validation.py
@@ -0,0 +1,37 @@
+import os
+from src.datascience_project import logger
+import pandas as pd
+from src.datascience_project.entity.config_entity import DataValidationConfig
+from src.datascience_project.config.configuration import ConfigurationManager
+
+class DataValidation:
+    def __init__(self,config:DataValidationConfig):
+        self.config=config
+    
+    def validate_all_columns(self)->bool:
+        try:
+            validation_status=None
+
+            data=pd.read_csv(self.config.unzip_data_dir)
+            all_columns=list(data.columns)
+            all_schema=self.config.all_schema.keys()
+
+            for column in all_columns:
+                if column not in all_schema:
+                    validation_status=False
+                    with open(self.config.STATUS_FILE,'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+
+                else:
+                    validation_status=True
+                    with open(self.config.STATUS_FILE,'w') as f:
+                        f.write(f"Validation status: {validation_status}")
+
+
+            return validation_status
+        
+        except Exception as e:
+            raise e
+        
+
+
